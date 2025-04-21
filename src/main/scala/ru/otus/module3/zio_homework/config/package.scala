419b2598558bec64966f2e06b018bc1518fc1184
Aleksey Yakushev <yakushev@proitr.ru>
2025-04-22 00:27:20 +0500

    Homework-8 v1

--- src/main/scala/ru/otus/module3/zio_homework/config/package.scala
+++ src/main/scala/ru/otus/module3/zio_homework/config/package.scala
@@ -3,12 +3,8 @@ package ru.otus.module3.zio_homework
 import zio.config.magnolia._
 import zio.{Config, ConfigProvider, IO}
 
-
 package object config {
    case class AppConfig(host: String, port: String)
-
-
-
 
   private val myConfigAutomatic: Config[AppConfig] = deriveConfig[AppConfig]
 
